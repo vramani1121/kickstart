4aca2febcf56a425c9ebdbe0847f88ef0bafeb49
vivek ramani <92964991+vramani1121@users.noreply.github.com>
2022-10-19 09:17:12 +0530

    Update web3.js

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -10,7 +10,7 @@ web3 = new Web3(window.ethereum);
 }
 
 else{
-    const provider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/042135ce7aef4fbe9a0fa6c9f85d0708');
+    const provider = new Web3.providers.HttpProvider('https://goerli.infura.io/v3/042135ce7aef4fbe9a0fa6c9f85d0708');
     web3 = new Web3(provider);
 }
 
